553607e2a8f0f545e7707397066b4aff91dab03f
Mike Abbott <furrysalamander@gmail.com>
2018-11-02 00:54:04 -0600

    Added all of the files
    Meant to do this in the initial commit.  Oops...

---
+++ LC3Simulator/LC.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC3Simulator
+{
+    class LC
+    {
+        public short[] registers = new short[8];
+        public short[] memory = new short[5000];
+        public bool halt;
+        public short programCounter;
+        bool[] nzp = new bool[3];
+
+        public char outChar;
+        public bool outCharFlag;
+
+        public LC()
+        {
+            programCounter = 0;
+        }
+
+        public void ParseMachineCode(short command)
+        {
+            outCharFlag = false;
+
+            short opcode = (short)((command >> 12) & 0b1111);
+            int DR = (short)((command >> 9) & 0b111);
+            int SR = (short)((command >> 6) & 0b111);
+            int SR2 = (short)(command & 0b111);
+
+            short imm5 = (short)(command << 11);
+            imm5 = (short)(imm5 >> 11);
+            short pc9 = (short)(command << 7);
+            pc9 = (short)(pc9 >> 7);
+            short o6 = (short)(command << 10);
+            o6 = (short)(o6 >> 10);
+
+            
+
+            switch (opcode)
+            {
+                case 0b0001: // ADD
+                    if ((command & 0b100000) == '0')
+                    {
+                        registers[DR] = (short)(registers[SR] + registers[SR2]);
+                    }
+                    else
+                    {
+                        registers[DR] = (short)(registers[SR] + imm5);
+                    }
+                    SetFlags(registers[DR]);
+                    break;
+                case 0b0101: // AND
+                    if ((command & 0b100000) == '0')
+                    {
+                        registers[DR] = (short)(registers[SR] & registers[SR2]);
+                    }
+                    else
+                    {
+                        registers[DR] = (short)(registers[SR] & imm5);
+                    }
+                    SetFlags(registers[DR]);
+                    break;
+                case 0b0000: // BR
+                    // decodes NZP flags
+                    bool n = (DR & 0b100) != 0;
+                    bool z = (DR & 0b010) != 0;
+                    bool p = (DR & 0b001) != 0;
+                    // checks to see if any of the NZP flags have been met before executing the branch
+                    if ((nzp[0] && n )|| (nzp[1] && z) || (nzp[2] && p) || (n && p && z))
+                    {
+                        programCounter += pc9;
+                        return;
+                    }
+                    break;
+                case 0b1100: // JMP and RET
+                    programCounter = registers[SR];
+                    return;
+                case 0b0100: // JSR and JSRR
+                    if ((DR & 0b100) == 1)
+                    {
+                        short o11 = (short)(command << 5);
+                        programCounter += (short)(o11 >> 5);
+                    }
+                    else
+                        programCounter = registers[SR];
+                    return;
+                case 0b0010: // LD
+                    registers[DR] = memory[programCounter + pc9];
+                    break;
+                case 0b1010: // LDI
+                    registers[DR] = memory[memory[programCounter + pc9]];
+                    break;
+                case 0b0110: // LDR
+                    registers[DR] = memory[registers[SR] + o6];
+                    break;
+                case 0b1110: // LEA
+                    registers[DR] = (short)(programCounter + pc9);
+                    break;
+                case 0b1001: // NOT
+                    registers[DR] = (short)(~registers[SR]);
+                    SetFlags(registers[DR]);
+                    break;
+                case 0b1000: // RTI
+                    
+                    // IMPLEMENT
+
+                    break;
+                case 0b0011: // ST
+                    memory[programCounter + pc9] = registers[DR];
+                    break;
+                case 0b1011: // STI
+                    memory[memory[programCounter + pc9]] = registers[DR];
+                    break;
+                case 0b0111: // STR
+                    memory[registers[SR] + o6] = registers[DR];
+                    break;
+                case 0b1111: // TRAP
+                    switch (command & 0b11111111)
+                    {
+                        case 0x25: // Halt Vector
+                            halt = true;
+                            break;
+                        case 0x23: // Input Character
+                            break;
+                        case 0x21: // Output Character
+                            outChar = (char)registers[0];
+                            outCharFlag = true;
+                            break;
+                    }
+                    break;
+                case 0b1101: // RESERVED
+
+                    break;
+            }
+            programCounter += 1;
+        }
+        
+
+        // No longer needed, sign extension is done with bitshifts.
+        /*private short Sext(string inInt)
+        {
+            inInt.PadLeft(16, inInt[0]);
+            return Convert.Toshort(inInt, 2);
+        }//*////*
+
+        private void SetFlags(int value)
+        {
+            nzp[0] = value < 0;
+            nzp[1] = value == 0;
+            nzp[2] = value > 0;
+        }
+    }
+}
---
+++ LC3Simulator/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace LC3Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LC computer = new LC();
+
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+
+            // computer.r1 = 5;
+        }
+
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+            computer.programCounter = 0;
+            computer.halt = false;
+            string inProgram = StringFromRichTextBox(program);
+
+            string[] commands = Regex.Split(inProgram, "\r\n");
+            Array.Resize<string>(ref commands, commands.Length - 1); // Removes the empty line at end of array
+
+
+            foreach (string command in commands)
+            {
+                foreach (char test in command)
+                {
+                    if ((test != '1') && (test != '0'))
+                    {
+                        MessageBox.Show("Invalid characters in input! (Character: " + test + ')');
+                        return;
+                    }
+                }
+                if (command.Length != 16)
+                {
+                    MessageBox.Show("Input has invalid command lengths!");
+                    return;
+                }
+            }
+            for (int i = 0; i < commands.Length; i++)
+            {
+                computer.memory[i] = Convert.ToInt16(commands[i], 2);
+            }
+            string consoleText = "";
+            while (!computer.halt && (computer.programCounter < computer.memory.Length))
+            {
+                computer.ParseMachineCode(computer.memory[computer.programCounter]);
+                prgCounter.Text = Convert.ToString(computer.programCounter);
+                if (computer.outCharFlag)
+                {
+                    consoleText += computer.outChar;
+                }
+                // consoleOut.Text = computer.registers[1].ToString();
+            }
+            consoleOut.Text = consoleText;
+            if (computer.halt) consoleOut.Text += " Halted!";
+            else consoleOut.Text += " End of memory!";
+        }
+
+        string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                rtb.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                rtb.Document.ContentEnd
+            );
+
+            // The Text property on a TextRange object returns a string
+            // representing the plain text content of the TextRange.
+            return textRange.Text;
+        }
+
+        private void program_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            consoleOut.Text = "";
+        }
+    }
+}
